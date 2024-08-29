d5e47487eb4087414014d3af0e326dc374264224
sangshrestha <sang.socials@gmail.com>
2024-08-29 21:21:00 +0100

    feat: animations

---
+++ index.js
@@ -0,0 +1,58 @@
+const SECOND = 1000; // in ms
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const MONTH = 30 * DAY;
+
+const START_DATE = new Date("2021-05-20");
+
+const yearsSince = new Date().getUTCFullYear() - START_DATE.getUTCFullYear();
+
+const updatedDate = START_DATE.setFullYear(
+  START_DATE.getFullYear() + yearsSince,
+);
+
+const differenceInMs = Date.now() - updatedDate;
+const monthsRemaining = Math.round(differenceInMs / MONTH);
+const secondsRemaining = Math.round(
+  (differenceInMs - monthsRemaining) / SECOND,
+);
+
+const versionSpan = document.querySelector("#version");
+versionSpan.innerText = `${yearsSince}.${monthsRemaining}.${secondsRemaining}`;
+
+// animation for drawing terminal lines
+const lines = [...document.querySelectorAll("p:not(.show)")];
+
+const inputAnimationDelayTotal = 0;
+
+function wait(s) {
+  return new Promise((res) => setTimeout(res, s));
+}
+
+async function drawLines(lines) {
+  for (const line of lines) {
+    await wait(250);
+
+    if (line.classList.contains("tprompt")) {
+      const inputSpan = line.querySelector(".tprompt__input");
+
+      if (inputSpan) {
+        const inputContent = [...inputSpan.innerHTML];
+        inputSpan.innerHTML = "";
+        line.classList.add("show");
+
+        for (const char of inputContent) {
+          await wait(100);
+          inputSpan.innerHTML += char;
+        }
+      } else {
+        line.classList.add("show");
+      }
+    } else {
+      line.classList.add("show");
+    }
+  }
+}
+
+drawLines(lines);
